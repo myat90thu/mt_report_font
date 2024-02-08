827386d0fd00686ba289412e745c2d88016f50fa
MYAT THU <myatthu@myansis.com>
2024-02-08 13:13:12 +0630

    [ADD] Initial Commit

---
+++ mt_report_font/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Wkhtmltopdf Myanmar Font Rendering",
+    "version": "15.0.0.0.1",
+    "author": "MYAT THU [Odoo Myanmar Tutorial]",
+    "website": "https://myat90thu.github.io",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Base module for mm font rendering in qweb report",
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+
+}
